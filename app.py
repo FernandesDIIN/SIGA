4b6f621ded2e67f2507d7e9415abcaf37fc7da5d
Domingos <tchivangululafernandes@gmail.com>
2025-10-22 00:28:55 +0100

    Refatorado para layout.html e criada pagina de horario protegida

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
-# --- DADOS FICTÍCIOS (MAIS TARDE VIRÁ DA BASE DE DADOS) ---
+app.secret_key = 'minha-chave-secreta-para-o-tcc'
+
 anuncios_da_escola = [
     {
         "id": 1,
@@ -25,18 +26,45 @@ anuncios_da_escola = [
 ]
 # ---------------------------------------------------------
 
+horario_aluno = [
+    {"dia": "Segunda-feira", "hora": "08:00-09:30", "disciplina": "Matemática", "sala": "101"},
+    {"dia": "Segunda-feira", "hora": "10:00-11:30", "disciplina": "Física", "sala": "203"},
+    {"dia": "Terça-feira", "hora": "08:00-09:30", "disciplina": "Português", "sala": "105"},
+    {"dia": "Quarta-feira", "hora": "10:00-11:30", "disciplina": "Química", "sala": "201"},
+    {"dia": "Quinta-feira", "hora": "08:00-09:30", "disciplina": "História", "sala": "102"},
+    {"dia": "Sexta-feira", "hora": "10:00-11:30", "disciplina": "Educação Física", "sala": "Ginásio"}
+]
+
 
 @app.route('/')
 def homepage():
-    # Agora, vamos enviar a nossa lista de anúncios para o HTML
     return render_template('index.html', anuncios=anuncios_da_escola)
 
-# Define a rota para a página de login
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def pagina_login():
+    if request.method == 'POST':
+        utilizador = request.form['utilizador']
+        senha = request.form['senha']
+
+        if utilizador == 'aluno' and senha == '1234':
+            session['utilizador_logado'] = utilizador  
+            return redirect(url_for('homepage'))  
+
     return render_template('login.html')
 
 
-# Permite executar a aplicação
+@app.route('/logout')
+def logout():
+    session.pop('utilizador_logado', None)  
+    return redirect(url_for('homepage'))  
+
+@app.route('/horario')
+def pagina_horario():
+    if 'utilizador_logado' not in session:
+        return redirect(url_for('pagina_login'))
+
+    return render_template('horario.html', horario=horario_aluno)
+
 if __name__ == '__main__':
     app.run(debug=True)
